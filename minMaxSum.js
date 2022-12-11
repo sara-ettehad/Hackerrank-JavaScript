4a3c3bfa161289a9df8a4403873c322124d9cf58
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2022-12-11 00:46:02 -0500

    message

---
+++ minMaxSum.js
@@ -0,0 +1,15 @@
+function miniMaxSum(arr) {
+    // Write your code here
+    let sortarr = arr.sort();
+    let min = 0;
+    let max = 0;
+    
+    for(let i = 0 ; i < arr.length-1 ; i++){
+        min += sortarr[i];
+    }
+    for(let j = arr.length-1 ; j > 0 ; j--){
+        max += sortarr[j];
+    }
+    console.log(min + " " + max);
+
+}
